b4867868c4c87c49a101792f10c3dc946ac67f11
sonymaker <sonymaker@naver.com>
2019-01-07 14:38:38 +0900

    배포파일 경로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'development',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public/dist'),
         filename: 'bundle.js'
     },
     devServer: {
